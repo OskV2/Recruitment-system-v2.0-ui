59dace3aaaa06d03e2a6f019692a50e1caffefa7
Maciej Buszkiewicz <maciek.buszkiewicz@ideeconcept.pl>
2024-03-09 16:56:27 +0100

    feat: added basic Next.js template to work on

---
+++ app/offers/page.jsx
@@ -0,0 +1,7 @@
+
+export default function Offer() {
+    return (
+      <h2>This is offer page</h2>
+    );
+  }
+  
---
+++ components/Navbar.tsx
@@ -0,0 +1,5 @@
+export default function Navbar() {
+    return (
+        <h1 className='text-red-500'>This is literally navbar</h1>
+    )
+}
